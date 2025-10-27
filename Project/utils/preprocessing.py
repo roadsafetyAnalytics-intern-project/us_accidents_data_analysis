83ff115b9eb37fdb988c0e4a0cfd35674e19c06f
springboardmentor942x-alt <springboardmentor942x@gmail.com>
2025-10-27 14:40:30 +0530

    Modified Project folder

--- Project/utils/preprocessing.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def clean_data(df):
-    df['Start_Time'] = pd.to_datetime(df['Start_Time'])
-    df = df.dropna(subset=['Weather_Condition', 'Visibility(mi)', 'Severity'])
-    df['Hour'] = df['Start_Time'].dt.hour
-    df['Month'] = df['Start_Time'].dt.month
-    df['Weekday'] = df['Start_Time'].dt.day_name()
-    return df
